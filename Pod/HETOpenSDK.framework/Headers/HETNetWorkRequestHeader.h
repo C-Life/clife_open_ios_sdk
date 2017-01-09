485517de1ca8a38164787e798fc1e8555f2a9a4a
open@clife.net <open@clife.net>
2017-01-09 17:49:48 +0800

    update  sdk

---
+++ Pod/HETOpenSDK.framework/Headers/HETNetWorkRequestHeader.h
@@ -0,0 +1,26 @@
+//
+//  HETNetWorkRequestHeader.h
+//  HETOpenSDK
+//
+//  Created by mr.cao on 16/3/16.
+//  Copyright © 2016年 mr.cao. All rights reserved.
+//
+
+#ifndef HETNetWorkRequestHeader_h
+#define HETNetWorkRequestHeader_h
+
+
+typedef NS_ENUM(NSInteger , HETRequestMethod) {
+    HETRequestMethodGet = 0,
+    HETRequestMethodPost,
+    HETRequestMethodHead,
+    HETRequestMethodPut,
+    HETRequestMethodDelete,
+    HETRequestMethodPatch
+};
+
+
+
+
+
+#endif /* HETNetWorkRequestHeader_h */
